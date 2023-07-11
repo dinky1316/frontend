a87623db7f6ed26e3d10ba2d514ec5e654de7f96
doumin <aishi1026@gmail.com>
2023-07-11 14:30:47 +0900

    table 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Home=()=> {
+   const [users, setUsers] = useState([]);
+
+   // 앱 실행시 한번 실행
+   useEffect(()=>{
+      loadUsers();
+   },[]);
+   // async는 만약 함수라면 function 앞에 붙임
+   const loadUsers = async () =>{
+      const result = await axios.get('http://localhost:8080/users');
+      setUsers(result.data)
+   }
+   return (
+     <div className="container">
+       <table className="table border text-center shadow my-4">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">이름</th>
+      <th scope="col">유저네임</th>
+      <th scope="col">이메일</th>
+    </tr>
+  </thead>
+  <tbody>
+   {users.map((user, index)=>(
+    <tr key={index}>
+      <th scope="row">{index+1}</th>
+      <td>{user.name}</td>
+      <td>{user.username}</td>
+      <td>{user.email}</td>
+    </tr>
+   ))}
+  </tbody>
+       </table>
+     </div>
+   );
+ }
+ 
+export default Home
