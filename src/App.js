eee61109103c593dfb4e0b8ee811ea57ad344306
doumin <aishi1026@gmail.com>
2023-07-11 15:34:32 +0900

    리액트 라우터

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddUser from "./users/AddUser";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* 이 주소(/)로 요청하면 home을 보여준다 */}
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
